f9370b9d1e1ebb8e26f3b286f5b9c054f4a5c397
Josh Lester <142093110+jeyeldotgit@users.noreply.github.com>
2023-10-23 23:18:58 +0800

    Add files via upload

---
+++ SW4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+int main (void)
+{
+        //Variable Initialization
+    int side_A[10];
+    int side_B[10];
+
+
+        //prompt for user input
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Measurement of Side A #%i: ", i + 1);
+        scanf("%i", &side_A[i]);
+    }
+
+
+    system("cls");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Measurement of Side B #%i: ",i + 1);
+        scanf("%i", &side_B[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        // Initialize length_A and length_B inside the loop
+        double length_A = (double)(side_A[i] * side_A[i]);
+        double length_B = (double)(side_B[i] * side_B[i]);
+        double hypotenuse = sqrt(length_A + length_B);
+        printf("Hypotenuse for set #%d: %lf\n", i + 1, hypotenuse);
+    }
+}
+
